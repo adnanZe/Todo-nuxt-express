8f45f29b55115111fa7898824760beed4b7169bb
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-18 16:46:52 +0200

    [feature] separated logic for tasks and auth in separate files

--- todo-backend/src/index.ts
+++ todo-backend/src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { authenticateToken, loginUser } from './modules/auth';
+import * as tasksModule from './modules/tasks';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, TypeScript!');
-});
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.post('/api/auth/login', loginUser);
+
+app.get('/api/tasks', authenticateToken, tasksModule.getTasks);
+app.post('/api/tasks', authenticateToken, tasksModule.createTask);
+app.put('/api/tasks/:id', authenticateToken, tasksModule.updateTask);
+app.delete('/api/tasks/:id', authenticateToken, tasksModule.deleteTask);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
