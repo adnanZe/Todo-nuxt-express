548e2bceb62124bae348baecb056a85c5059751a
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-19 15:27:52 +0200

    [wip] clean up nuxt project

--- todo-frontend/nuxt.config.js
+++ todo-frontend/nuxt.config.js
@@ -26,6 +26,7 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  srcDir: "src/",
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
